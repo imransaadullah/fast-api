924b6526ca50c7cc7be290c7294f7c1903005b3b
imransaadullah <imransaadullah@gmail.com>
2023-12-22 15:25:24 +0100

    Added to string replace method

--- src/Router.php
+++ src/Router.php
@@ -34,7 +34,8 @@ class Router
                     // If the handler is an array [$object, 'method'], invoke the method on the object
                     $object = $handler[0];
                     $method = $handler[1];
-                    $object->$method($request, ...$matches);
+                    // $object->$method($request, ...$matches);
+                    call_user_func_array([$object, $method], array_merge([$request], $matches))
                 } else {
                     // Handle unknown handler type
                     $response = new Response();
