3ccd252d654b641c6b9fd0789652520d080065a7
imransaadullah <imransaadullah@gmail.com>
2024-03-03 22:52:20 +0100

    Added CustomTime and Token Classes for time and jwt manipulation

---
+++ src/CustomTime/CustomTime.php
@@ -0,0 +1,439 @@
+<?php
+
+namespace API\CustomTime;
+
+use \DateTimeImmutable;
+use \DateTime;
+use \DateTimezone;
+use \DateInterval;
+
+/**
+ * Class CustomTime
+ * Represents a custom time object with additional functionalities.
+ */
+class CustomTime extends DateTimeImmutable{
+    /**
+     * @var int The timestamp representing the time associated with this object.
+     */
+    private $time;
+
+    /**
+     * @var DateTime The DateTime object representing the date and time associated with this object.
+     */
+    private $date;
+
+    /**
+     * @var DateTimeZone The timezone associated with this object.
+     */
+    private $timezone;
+
+    /**
+     * @var string The default format used for date formatting.
+     */
+    private $format = DateTimeImmutable::RFC7231;
+
+    /**
+     * Constructor.
+     *
+     * @param string $date A date/time string or DateTimeImmutable object. Defaults to 'now'.
+     */
+    public function __construct($date = 'now') {
+        $this->timezone = new DateTimezone($_ENV['TIMEZONE']);
+        $this->date = new parent($date, $this->timezone);
+        $this->time = $this->date->getTimestamp();
+    }
+
+    /**
+     * Gets the formatted date based on the specified format.
+     *
+     * @param string|null $format The format to use for formatting the date. Defaults to the class default format.
+     * @return string The formatted date string.
+     */
+    public function get_date($format = null) {
+        return $this->date->format($format ?? $this->format);
+    }
+
+    /**
+     * Gets the formatted time based on the specified format.
+     *
+     * @param string $format The format to use for formatting the time. Defaults to 'H:i:s'.
+     * @return string The formatted time string.
+     */
+    public function get_time($format = 'H:i:s') {
+        return $this->date->format($format);
+    }
+
+    /**
+     * Gets the formatted UTC time based on the specified format.
+     *
+     * @param string $format The format to use for formatting the time. Defaults to 'H:i:s'.
+     * @return string The formatted UTC time string.
+     */
+    public function get_utc_time($format = 'H:i:s') {
+        $utcDate = $this->date->setTimezone(new DateTimeZone('UTC'));
+        return $utcDate->format($format);
+    }
+
+    /**
+     * Gets the timestamp associated with this object.
+     *
+     * @return int The timestamp.
+     */
+    public function get_timestamp() {
+        return $this->date->getTimestamp();
+    }
+
+    /**
+     * Extends the date and time by the specified duration.
+     *
+     * @param int $days The number of days to add.
+     * @param int $hours The number of hours to add.
+     * @param int $minutes The number of minutes to add.
+     * @param int $seconds The number of seconds to add.
+     * @return CustomTime The modified CustomTime object.
+     */
+    public function extend_date($days = 0, $hours = 0, $minutes = 0, $seconds = 0) {
+        $interval = sprintf("P%dDT%dH%dM%dS", $days, $hours, $minutes, $seconds);
+        $newDate = $this->date->add(new DateInterval($interval));
+        $this->date = $newDate;
+        $this->time = $this->date->getTimestamp();
+        return $this;
+    }
+
+    /**
+     * Checks if this time is before another time.
+     *
+     * @param CustomTime $otherTime The other time to compare.
+     * @return bool True if this time is before the other time, false otherwise.
+     */
+    public function isBefore($otherTime) {
+        return $this->date < $otherTime->date;
+    }
+    
+    /**
+     * Checks if this time is after another time.
+     *
+     * @param CustomTime $otherTime The other time to compare.
+     * @return bool True if this time is after the other time, false otherwise.
+     */
+    public function isAfter($otherTime) {
+        return $this->date > $otherTime->date;
+    }
+    
+    /**
+     * Checks if this time is equal to another time.
+     *
+     * @param CustomTime $otherTime The other time to compare.
+     * @return bool True if this time is equal to the other time, false otherwise.
+     */
+    public function equals($otherTime) {
+        return $this->date == $otherTime->date;
+    }
+
+    /**
+     * Sets the timezone for this object.
+     *
+     * @param string|DateTimeZone $timezone The timezone identifier or DateTimeZone object.
+     * @return CustomTime The modified CustomTime object.
+     * @throws \InvalidArgumentException If an invalid timezone argument is provided.
+     */
+    public function set_timezone($timezone) {
+        if ($timezone instanceof DateTimeZone) {
+            $this->timezone = $timezone;
+        } elseif (is_string($timezone)) {
+            $this->timezone = new DateTimeZone($timezone);
+        } else {
+            throw new \InvalidArgumentException('Invalid timezone argument. Expected string or DateTimeZone object.');
+        }
+        $this->date = $this->date->setTimezone($this->timezone);
+        return $this;
+    }
+
+    /**
+     * Sets the default format used for date formatting.
+     *
+     * @param string $format The default format to set.
+     * @return void
+     */
+    public function set_format($format) {
+        $this->format = $format;
+    }
+
+    /**
+     * Formats the date and time using the specified format.
+     *
+     * @param string|null $format The format to use for formatting. Defaults to the class default format.
+     * @return string The formatted date and time string.
+     */
+    public function formatCustom($format = null) {
+        return $this->date->format($format ?? $this->format);
+    }
+
+    /**
+     * Calculates the difference in days between this time and another time.
+     *
+     * @param CustomTime $otherTime The other time to calculate the difference from.
+     * @return int The difference in days.
+     */
+    public function diffInDays($otherTime) {
+        $diff = $this->date->diff($otherTime->date);
+        return $diff->days;
+    }
+
+    /**
+     * Adds the specified number of days to this time.
+     *
+     * @param int $days The number of days to add.
+     * @return CustomTime The modified CustomTime object.
+     */
+    public function add_days($days) {
+        $this->date = $this->date->modify("+{$days} days");
+        return $this;
+    }
+    
+    /**
+     * Subtracts the specified number of days from this time.
+     *
+     * @param int $days The number of days to subtract.
+     * @return CustomTime The modified CustomTime object.
+     */
+    public function subtract_days($days) {
+        $this->date = $this->date->modify("-{$days} days$days");
+        return $this;
+    }
+
+    /**
+     * Adds the specified number of weeks to this time.
+     *
+     * @param int $weeks The number of weeks to add.
+     * @return CustomTime The modified CustomTime object.
+     */
+    public function add_weeks($weeks) {
+        $this->date = $this->date->modify("+{$weeks} weeks");
+        return $this;
+    }
+    
+    /**
+     * Subtracts the specified number of weeks from this time.
+     *
+     * @param int $weeks The number of weeks to subtract.
+     * @return CustomTime The modified CustomTime object.
+     */
+    public function subtract_weeks($weeks) {
+        $this->date = $this->date->modify("-{$weeks} weeks");
+        return $this;
+    }
+
+    /**
+     * Adds the specified number of months to this time.
+     *
+     * @param int $months The number of months to add.
+     * @return CustomTime The modified CustomTime object.
+     */
+    public function add_months($months) {
+        $this->date = $this->date->modify("+{$months} months");
+        return $this;
+    }
+    
+    /**
+     * Subtracts the specified number of months from this time.
+     *
+     * @param int $months The number of months to subtract.
+     * @return CustomTime The modified CustomTime object.
+     */
+    public function subtract_months($months) {
+        $this->date = $this->date->modify("-{$months} months");
+        return $this;
+    }
+
+    /**
+     * Adds the specified number of years to this time.
+     *
+     * @param int $years The number of years to add.
+     * @return CustomTime The modified CustomTime object.
+     */
+    public function add_years($years) {
+        $this->date = $this->date->modify("+{$years} years");
+        return $this;
+    }
+    
+    /**
+     * Subtracts the specified number of years from this time.
+     *
+     * @param int $years The number of years to subtract.
+     * @return CustomTime The modified CustomTime object.
+     */
+    public function subtract_years($years) {
+        $this->date = $this->date->modify("-{$years} years");
+        return $this;
+    }
+
+    /**
+     * Adds the specified number of minutes to this time.
+     *
+     * @param int $minutes The number of minutes to add.
+     * @return CustomTime The modified CustomTime object.
+     * @throws \InvalidArgumentException If the number of minutes is not an integer.
+     */
+    public function add_minutes($minutes) {
+        if (!is_int($minutes)) {
+            throw new \InvalidArgumentException('Number of minutes must be an integer.');
+        }
+
+        $this->date = $this->date->modify("+{$minutes} minutes");
+        return $this;
+    }
+
+    /**
+     * Adds the specified number of hours to this time.
+     *
+     * @param int $hours The number of hours to add.
+     * @return CustomTime The modified CustomTime object.
+     * @throws \InvalidArgumentException If the number of hours is not an integer.
+     */
+    public function add_hours($hours) {
+        if (!is_int($hours)) {
+            throw new \InvalidArgumentException('Number of hours must be an integer.');
+        }
+
+        $this->date = $this->date->modify("+{$hours} hours");
+        return $this;
+    }
+
+    /**
+     * Adds the specified number of seconds to this time.
+     *
+     * @param int $seconds The number of seconds to add.
+     * @return CustomTime The modified CustomTime object.
+     * @throws \InvalidArgumentException If the number of seconds is not an integer.
+     */
+    public function add_seconds($seconds) {
+        if (!is_int($seconds)) {
+            throw new \InvalidArgumentException('Number of seconds must be an integer.');
+        }
+
+        $this->date = $this->date->modify("+{$seconds} seconds");
+        return $this;
+    }
+
+    /**
+     * Subtracts the specified number of minutes from this time.
+     *
+     * @param int $minutes The number of minutes to subtract.
+     * @return CustomTime The modified CustomTime object.
+     * @throws \InvalidArgumentException If the number of minutes is not an integer.
+     */
+    public function subtract_minutes($minutes) {
+        if (!is_int($minutes)) {
+            throw new \InvalidArgumentException('Number of minutes must be an integer.');
+        }
+
+        $this->date = $this->date->modify("-{$minutes} minutes");
+        return $this;
+    }
+
+    /**
+     * Subtracts the specified number of hours from this time.
+     *
+     * @param int $hours The number of hours to subtract.
+     * @return CustomTime The modified CustomTime object.
+     * @throws \InvalidArgumentException If the number of hours is not an integer.
+     */
+    public function subtract_hours($hours) {
+        if (!is_int($hours)) {
+            throw new \InvalidArgumentException('Number of hours must be an integer.');
+        }
+
+        $this->date = $this->date->modify("-{$hours} hours");
+        return $this;
+    }
+
+    /**
+     * Subtracts the specified number of seconds from this time.
+     *
+     * @param int $seconds The number of seconds to subtract.
+     * @return CustomTime The modified CustomTime object.
+     * @throws \InvalidArgumentException If the number of seconds is not an integer.
+     */
+    public function subtract_seconds($seconds) {
+        if (!is_int($seconds)) {
+            throw new \InvalidArgumentException('Number of seconds must be an integer.');
+        }
+
+        $this->date = $this->date->modify("-{$seconds} seconds");
+        return $this;
+    }
+
+    /**
+     * Serializes the CustomTime object to a JSON representation.
+     *
+     * @return string The JSON representation of the object.
+     */
+    public function serialize() {
+        return json_encode([
+            'date' => $this->date->format('Y-m-d H:i:s'),
+            'timezone' => $this->date->getTimezone()->getName()
+        ]);
+    }
+
+    /**
+     * Deserializes a JSON representation into a CustomTime object.
+     *
+     * @param string $data The JSON data to deserialize.
+     * @return CustomTime The deserialized CustomTime object.
+     * @throws \InvalidArgumentException If the serialized data is invalid or cannot be deserialized.
+     */
+    public static function deserialize($data) {
+        $decodedData = json_decode($data, true);
+
+        if ($decodedData === null || !isset($decodedData['date'], $decodedData['timezone'])) {
+            throw new \InvalidArgumentException('Invalid serialized data');
+        }
+
+        $timezone = new DateTimeZone($decodedData['timezone']);
+        $dateTime = DateTimeImmutable::createFromFormat('Y-m-d H:i:s', $decodedData['date'], $timezone);
+
+        if ($dateTime === false) {
+            throw new \InvalidArgumentException('Invalid date format in serialized data');
+        }
+
+        return new CustomTime($dateTime->format('Y-m-d H:i:s'));
+    }
+
+    /**
+     * Adds a DateInterval to the CustomTime object.
+     *
+     * @param DateInterval $interval The interval to add.
+     * @return CustomTime The modified CustomTime object.
+     */
+    public function __add($interval) {
+        if ($interval instanceof DateInterval) {
+            $this->date = $this->date->add($interval);
+            $this->time = $this->date->getTimestamp();
+        }
+        return $this;
+    }
+    
+    /**
+     * Subtracts a DateInterval from the CustomTime object.
+     *
+     * @param DateInterval $interval The interval to subtract.
+     * @return CustomTime The modified CustomTime object.
+     */
+    public function __sub($interval) {
+        if ($interval instanceof DateInterval) {
+            $this->date = $this->date->sub($interval);
+            $this->time = $this->date->getTimestamp();
+        }
+        return $this;
+    }
+    
+    /**
+     * Converts the CustomTime object to a string representation.
+     *
+     * @return string The string representation of the CustomTime object.
+     */
+    public function __toString() {
+        return $this->format(DateTimeImmutable::RFC7231);
+    }
+}
---
+++ src/Token/Token.php
@@ -0,0 +1,324 @@
+<?php
+
+namespace API\Token;
+
+use \Firebase\JWT\JWT;
+use API\CustomTime\CustomTime;
+
+/**
+ * Class Token
+ * Handles JWT generation and verification.
+ */
+class Token extends JWT
+{
+    // Properties
+
+    /**
+     * @var string Path to the private key file.
+     */
+    private $private_key_file = $_ENV['SECRETS_DIR'] . "private.pem";
+
+    /**
+     * @var string Path to the public key file.
+     */
+    private $public_key_file = $_ENV['SECRETS_DIR'] . "public.pem";
+
+    /**
+     * @var string Timezone used for token generation.
+     */
+    private $timezone;
+
+    /**
+     * @var int Token not-before timestamp.
+     */
+    private $not_before;
+
+    /**
+     * @var int Token expiry timestamp.
+     */
+    private $expiry;
+
+    /**
+     * @var string Token issuer.
+     */
+    private $issuer;
+
+    /**
+     * @var string Token audience.
+     */
+    private $audience;
+
+    /**
+     * @var CustomTime Custom time handler for token generation.
+     */
+    private $custom_time;
+
+    /**
+     * @var string The generated token.
+     */
+    private $token;
+
+    /**
+     * @var mixed Decoded token data.
+     */
+    private $data;
+
+    /**
+     * @var string Secret key used for HMAC encryption.
+     */
+    private $secret_key;
+
+    /**
+     * @var string Algorithm used for token encryption.
+     */
+    private $algo = 'RS256';
+
+    /**
+     * @var bool Whether to use SSL for token encryption.
+     */
+    private $use_ssl = false;
+
+    // Methods
+    /**
+     * Constructor.
+     * @param string $audience The audience for the token.
+     * @param CustomTime|null $timeHandler Custom time handler.
+     * @param bool $use_ssl Whether to use SSL.
+     * @throws \Exception If secret key is not set or private key file is missing.
+     */
+    public function __construct($audience, CustomTime $timeHnadler = null, bool $use_ssl = true)
+    {
+        if (empty($audience)) {
+            throw new \InvalidArgumentException('Audience cannot be empty.');
+        }
+
+        $this->use_ssl = $use_ssl;
+
+        if (!$use_ssl) {
+            $this->secret_key = $_ENV['SECRET_KEY'] ?? null;
+        }
+
+        $this->timezone = $_ENV['TIMEZONE'];
+        $this->issuer = $_ENV['TOKEN_ISSUER'];
+        $this->audience = $audience;
+        $this->custom_time = $timeHnadler ?? new CustomTime();
+        $this->not_before = $this->custom_time->get_timestamp();
+        $this->expiry = ($timeHnadler ?? new CustomTime())->add_minutes(5)->get_timestamp();
+    }
+
+    /**
+     * Retrieves the public key resource using OpenSSL.
+     * @return resource|false Returns the public key resource or false on failure.
+     */
+    protected function get_public_key_openssl()
+    {
+        return openssl_get_publickey($this->public_key_file);
+    }
+
+    /**
+     * Retrieves the private key resource using OpenSSL.
+     * @return resource|false Returns the private key resource or false on failure.
+     */
+    protected function get_private_key_openssl()
+    {
+        return openssl_get_privatekey($this->private_key_file);
+    }
+
+    /**
+     * Sets the path to the private key file for OpenSSL.
+     * @param string|null $file The path to the private key file.
+     */
+    public function set_private_key_file_openssl($file = null)
+    {
+        $this->private_key_file = $file ?? $_ENV['SECRETS_DIR'] . 'private.pem';
+    }
+
+    /**
+     * Sets the path to the public key file for OpenSSL.
+     * @param string|null $file The path to the public key file.
+     */
+    public function set_public_key_file_openssl($file = null)
+    {
+        $this->public_key_file = $file ?? $_ENV['SECRETS_DIR'] . 'public.pem';
+    }
+
+    /**
+     * Sets the secret key for HMAC-based encryption.
+     * @param string $secret_key The secret key.
+     */
+    public function set_secret_key(string $secret_key)
+    {
+        $this->secret_key = $secret_key;
+    }
+
+    /**
+     * Retrieves the secret key for HMAC-based encryption.
+     * @return string|null The secret key or null if not set.
+     */
+    protected function get_secret_key()
+    {
+        return $this->secret_key;
+    }
+
+    /**
+     * Sets the algorithm for token generation.
+     * @param string $algo The algorithm name.
+     */
+    public function set_algo($algo)
+    {
+        $this->algo = $algo;
+    }
+
+    /**
+     * Retrieves the algorithm for token generation.
+     * @return string The algorithm name.
+     */
+    protected function get_algo()
+    {
+        return $this->algo;
+    }
+
+    /**
+     * Generates a JWT token with the provided data and expiry.
+     * @param array $data The data payload for the token.
+     * @param string $expiry The expiry timestamp for the token.
+     * @return string The generated JWT token.
+     * @throws \Exception If secret key is not set or private key file is missing.
+     */
+    public function make(array $data = [], $expiry = ''): self
+    {
+        if (!$this->use_ssl && !$this->secret_key)
+            throw new \Exception('Secret Key was not set');
+        if ($this->use_ssl && !is_file($this->private_key_file))
+            throw new \Exception('Private key is missing or invalid');
+
+        $token = [
+            "iss" => $this->issuer,
+            "aud" => $this->audience,
+            "iat" => $this->custom_time->get_timestamp(),
+            "nbf" => $this->custom_time->get_timestamp(),
+            "exp" => $expiry ?? $this->expiry,
+            "data" => $data
+        ];
+
+        if (!$this->use_ssl && $this->secret_key)
+            $this->token = $this->encode($token, $this->secret_key);
+        else if ($this->use_ssl && is_file($this->private_key_file)) {
+            $this->token = $this->encode($token, $this->get_private_key_openssl(), $this->algo);
+        } else {
+            throw new \Exception("Unable to generate token due to bad configuration");
+        }
+
+        return $this;
+    }
+
+    /**
+     * Verifies a JWT token and returns the decoded data.
+     * @param string $token The JWT token to verify.
+     * @return $this The Token instance.
+     * @throws \Exception If secret key is not set or private key file is missing.
+     */
+    public function verify($token): self
+    {
+        if (!$this->use_ssl && !$this->secret_key)
+            throw new \Exception('Secret Key was not set');
+        if ($this->use_ssl && !is_file($this->private_key_file))
+            throw new \Exception('Private key can not be located');
+
+        if (!$this->use_ssl && $this->secret_key)
+            $this->data = $this->decode($token, $this->secret_key);
+        else if ($this->use_ssl && is_file($this->public_key_file)) {
+            $this->data = $this->decode($token, $this->get_public_key_openssl(), [$this->algo]);
+        } else {
+            throw new \Exception("Unable to verify token due to bad configuration");
+        }
+
+        return $this;
+    }
+
+    /**
+     * Sets the issuer claim (`iss`) in the JWT payload.
+     * @param string $issuer The issuer value.
+     */
+    public function set_issuer(string $issuer) {
+        $this->issuer = $issuer;
+    }
+
+    /**
+     * Sets the audience claim (`aud`) in the JWT payload.
+     * @param string $audience The audience value.
+     */
+    public function set_audience(string $audience) {
+        $this->audience = $audience;
+    }
+
+    /**
+     * Sets the not-before claim (`nbf`) in the JWT payload.
+     * @param int $timestamp The not-before timestamp.
+     */
+    public function set_not_before(int $timestamp) {
+        $this->not_before = $timestamp;
+    }
+
+    /**
+     * Adds a custom claim to the JWT payload.
+     * @param string $name The name of the custom claim.
+     * @param mixed $value The value of the custom claim.
+     */
+    public function add_claim(string $name, $value) {
+        $this->data[$name] = $value;
+    }
+
+    /**
+     * Checks whether the token has expired based on its expiry claim (`exp`).
+     * @param string $token The JWT token to check.
+     * @return bool True if the token has expired, false otherwise.
+     */
+    public function is_expired(string $token): bool {
+        if ($this->verify($token)) {
+            return time() > $this->data->exp;
+        }
+        return false;
+    }
+
+    /**
+     * Refreshes the token by extending its expiry without changing its contents.
+     * @param string $token The JWT token to refresh.
+     * @param int $expiryDelta The amount of time (in seconds) to extend the expiry.
+     * @return string The refreshed JWT token.
+     */
+    public function refresh(string $token, int $expiryDelta): string {
+        if ($this->verify($token)) {
+            $this->make((array) $this->data->data, time() + $expiryDelta);
+        }
+
+        throw new \Exception("Token does not contain expiry claim.");
+    }
+
+    /**
+     * Retrieves the decoded data from the verified JWT token.
+     * @return array|null The decoded data payload or null if token not verified.
+     */
+    public function get_data()
+    {
+        return $this->data ? $this->data->data : $this->data;
+    }
+
+    /**
+     * Retrieves the generated JWT token.
+     * @return string|null .
+     */
+    public function get_token() : string
+    {
+        return $this->token;
+    }
+
+    /**
+     * Sets the expiry timestamp for token generation.
+     * @param int $timestamp The expiry timestamp.
+     */
+    public function set_expiry($timestamp)
+    {
+        $this->expiry = $timestamp;
+    }
+}
