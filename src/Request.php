44153eb2adcbd95c2849da7570cfc4386b795985
imransaadullah <imransaadullah@gmail.com>
2024-03-04 17:15:08 +0100

    Added Header information extration to request

--- src/Request.php
+++ src/Request.php
@@ -7,11 +7,14 @@ class Request
     private $uri;
     private $data; 
 
+    private $headers;
+
     public function __construct($method, $uri, $data = [])
     {
         $this->method = $method;
         $this->uri = $uri;
         $this->data = $data;
+        $this->getHeaders();
     }
 
     public function getMethod()
@@ -31,5 +34,20 @@ class Request
 
     public function getFiles() {
         return $_FILES;
+    }
+
+    private function getHeaders()
+    {
+        $headers = [];
+        foreach (getallheaders() as $name => $value) {
+            $headers[$name] = $value;
+        }
+        $this->$headers = $headers;
+    }
+
+    public function getHeader($key)
+    {
+        $headers = $this->headers;
+        return $headers[$key] ?? null;
     }
 }
