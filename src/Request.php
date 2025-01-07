aa3487446edcefd622575b3758e2ebab52f67710
imransaadullah <imransaadullah@gmail.com>
2025-01-07 02:18:10 +0100

    Enhancement: Added more methods to request class

--- src/Request.php
+++ src/Request.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace FASTAPI;
 
 /**
@@ -10,7 +11,9 @@ class Request
 {
     private $method;
     private $uri;
-    private $data; 
+    private $data;
+    private $properties = []; // Dynamic properties storage
+    private $dynamicHeaders = []; // For dynamically added headers
 
     /**
      * Request constructor.
@@ -61,7 +64,8 @@ class Request
      *
      * @return array The uploaded files.
      */
-    public function getFiles() {
+    public function getFiles()
+    {
         return $_FILES;
     }
 
@@ -76,7 +80,7 @@ class Request
         foreach (getallheaders() as $name => $value) {
             $headers[$name] = $value;
         }
-        return $headers;
+        return array_merge($headers, $this->dynamicHeaders);
     }
 
     /**
@@ -88,5 +92,167 @@ class Request
     public function getHeader($key)
     {
         return $this->getHeaders()[$key] ?? null;
+    }
+
+    /**
+     * Adds or updates a header.
+     *
+     * @param string $key The header key.
+     * @param string $value The header value.
+     */
+    public function withHeader($key, $value)
+    {
+        $this->dynamicHeaders[$key] = $value;
+    }
+
+    /**
+     * Removes a header.
+     *
+     * @param string $key The header key.
+     */
+    public function removeHeader($key)
+    {
+        unset($this->dynamicHeaders[$key]);
+    }
+
+    /**
+     * Sets a dynamic attribute.
+     *
+     * @param string $name The attribute name.
+     * @param mixed $value The attribute value.
+     */
+    public function setAttribute($name, $value)
+    {
+        $this->properties[$name] = $value;
+    }
+
+    /**
+     * Gets a dynamic attribute.
+     *
+     * @param string $name The attribute name.
+     * @return mixed|null The attribute value, or null if it does not exist.
+     */
+    public function getAttribute($name)
+    {
+        return $this->properties[$name] ?? null;
+    }
+
+    /**
+     * Magic method to dynamically set a property.
+     *
+     * @param string $name The property name.
+     * @param mixed $value The property value.
+     */
+    public function __set($name, $value)
+    {
+        $this->setAttribute($name, $value);
+    }
+
+    /**
+     * Magic method to dynamically get a property.
+     *
+     * @param string $name The property name.
+     * @return mixed|null The property value, or null if it does not exist.
+     */
+    public function __get($name)
+    {
+        return $this->getAttribute($name);
+    }
+
+    /**
+     * Magic method to check if a property is set.
+     *
+     * @param string $name The property name.
+     * @return bool True if the property is set, false otherwise.
+     */
+    public function __isset($name)
+    {
+        return isset($this->properties[$name]);
+    }
+
+    /**
+     * Magic method to unset a property.
+     *
+     * @param string $name The property name.
+     */
+    public function __unset($name)
+    {
+        unset($this->properties[$name]);
+    }
+
+    /**
+     * Checks if a dynamic attribute exists.
+     *
+     * @param string $name The attribute name.
+     * @return bool True if the attribute exists, false otherwise.
+     */
+    public function hasAttribute($name)
+    {
+        return array_key_exists($name, $this->properties);
+    }
+
+    /**
+     * Converts the Request object to an associative array.
+     *
+     * @return array An array representation of the Request.
+     */
+    public function toArray()
+    {
+        return [
+            'method' => $this->method,
+            'uri' => $this->uri,
+            'data' => $this->data,
+            'properties' => $this->properties,
+            'headers' => $this->getHeaders(),
+            'files' => $this->getFiles()
+        ];
+    }
+
+    /**
+     * Validates the request data based on provided rules.
+     *
+     * @param array $rules Validation rules (e.g., ['key' => 'required|string']).
+     * @return array|null An array of validation errors, or null if valid.
+     */
+    public function validateData(array $rules)
+    {
+        $errors = [];
+        foreach ($rules as $key => $rule) {
+            if (!isset($this->data[$key]) && strpos($rule, 'required') !== false) {
+                $errors[$key][] = 'This field is required.';
+            } elseif (isset($this->data[$key])) {
+                if (strpos($rule, 'string') !== false && !is_string($this->data[$key])) {
+                    $errors[$key][] = 'This field must be a string.';
+                }
+                if (strpos($rule, 'integer') !== false && !is_int($this->data[$key])) {
+                    $errors[$key][] = 'This field must be an integer.';
+                }
+            }
+        }
+        return !empty($errors) ? $errors : null;
+    }
+
+    /**
+     * Retrieves a query parameter from the URI.
+     *
+     * @param string $key The query parameter key.
+     * @return string|null The query parameter value, or null if not found.
+     */
+    public function getQueryParam($key)
+    {
+        $queryParams = [];
+        parse_str(parse_url($this->uri, PHP_URL_QUERY), $queryParams);
+        return $queryParams[$key] ?? null;
+    }
+
+    /**
+     * Retrieves JSON-encoded data from the request body.
+     *
+     * @return array|null The JSON-decoded data, or null if decoding fails.
+     */
+    public function getJsonData()
+    {
+        $json = file_get_contents('php://input');
+        return json_decode($json, true) ?? null;
     }
 }
